269f49a798a8494243447105bc12d24d2e36c57c
tandatproject <101375954+tandatproject@users.noreply.github.com>
2022-06-20 05:58:43 +0700

    checkIn congTy v3
     checkIn congTy v3

--- hr_attendance_location_ip_mac_address/static/src/js/attendance.js
+++ hr_attendance_location_ip_mac_address/static/src/js/attendance.js
@@ -31,8 +31,8 @@ odoo.define('hr_attendance_location_ip_mac_address.attendance', function(require
             };
             if (window.location.protocol == "https:") {
                 $.get('https://app2.jeoway.net/35/odoo_check_in_out/getIP_and_Location.php', function(data) {
-                    ipaddress = data.ipADD
-                    location_address = [data.diadiem, data.regionName, data.zip, data.country]
+                    ipaddress = data.query
+                    location_address = [data.city, data.regionName, data.zip, data.country]
                     set_ipaddress_location(ipaddress, location_address)
                 });
             } else {
@@ -70,8 +70,8 @@ odoo.define('hr_attendance_location_ip_mac_address.attendance', function(require
 
                 if (window.location.protocol == "https:") {
                     $.get('https://app2.jeoway.net/35/odoo_check_in_out/getIP_and_Location.php', function(data) {
-                        ipaddress = data.ipADD
-                        location_address = [data.diadiem, data.regionName, data.zip, data.country]
+                        ipaddress = data.query
+                        location_address = [data.city, data.regionName, data.zip, data.country]
                         set_ipaddress_location(ipaddress, location_address)
                     });
                 } else {
@@ -108,8 +108,8 @@ odoo.define('hr_attendance_location_ip_mac_address.attendance', function(require
                 };
                 if (window.location.protocol == "https:") {
                     $.get('https://app2.jeoway.net/35/odoo_check_in_out/getIP_and_Location.php', function(data) {
-                        ipaddress = data.ipADD
-                        location_address = [data.diadiem, data.regionName, data.zip, data.country]
+                        ipaddress = data.query
+                        location_address = [data.city, data.regionName, data.zip, data.country]
                         set_ipaddress_location(ipaddress, location_address)
                     });
                 } else {
